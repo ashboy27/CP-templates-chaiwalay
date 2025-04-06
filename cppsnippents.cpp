3f4c53cf6744b6946be19362d84ab0613d8ae54f
ashboy27 <120490848+ashboy27@users.noreply.github.com>
2025-04-06 17:07:01 +0500

    Added debugging

--- cppsnippents.cpp
+++ cppsnippents.cpp
@@ -1,18 +1,114 @@
+<snippet>
+  <content><![CDATA[
 /*
 *    Author: asharusmani
 *    Created: $CURRENT_DATETIME
+*
+*    ❝ If the world was ending, I'd wanna be next to you ❞
+*    ❝ If the party was over and our time on Earth was through ❞
+*    ❝ I'd hold you close for just a while... and die with a smile ❞
 */
-//Ya to win hai ya learn hai
-/*#ifndef ONLINE_JUDGE
-    #define DEBUG
-    #include "utilities.h"
-#else
-    #define debug(var)
-    #define debugArr(...)
-#endif*/
 
-#include <bits/stdc++.h>
+
+
+#include "bits/stdc++.h"
 using namespace std;
+#ifndef ONLINE_JUDGE
+    #define DEBUG // Enable debug locally
+#endif
+
+#ifdef DEBUG
+    #define msg(statement) cerr << statement << endl
+    #define debug(var) cerr << (#var) << ": " << var << endl
+#else
+    #define msg(statement)
+    #define debug(var)
+#endif
+
+// Template aliases for convenience
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+// Ostream overloads for printing (always available, minimal compile-time cost)
+template <typename T>
+ostream &operator<<(ostream &os, min_heap<T> H) {
+    while (!H.empty()) {
+        os << H.top() << " ";
+        H.pop();
+    }
+    return os << endl;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, max_heap<T> H) {
+    while (!H.empty()) {
+        os << H.top() << " ";
+        H.pop();
+    }
+    return os << endl;
+}
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, pair<F, S> P) {
+    return os << P.first << " " << P.second;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> arr) {
+    for (int i = 0; i < (int)arr.size(); i++) {
+        os << arr[i] << " ";
+    }
+    return os << "";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<vector<T>> matrix) {
+    os << endl;
+    for (int i = 0; i < (int)matrix.size(); i++) {
+        for (int j = 0; j < (int)matrix[i].size(); j++) {
+            os << matrix[i][j] << " ";
+        }
+        os << endl;
+    }
+    return os << "";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, set<T> S) {
+    for (auto s : S) {
+        os << s << " ";
+    }
+    return os << "";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, multiset<T> S) {
+    for (auto s : S) {
+        os << s << " ";
+    }
+    return os << "";
+}
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, map<F, S> M) {
+    os << endl;
+    for (auto m : M) {
+        os << m << endl;
+    }
+    return os << "";
+}
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, multimap<F, S> M) {
+    os << endl;
+    for (auto m : M) {
+        os << m << endl;
+    }
+    return os << "";
+}
 #define ll long long
 #define nl endl
 #define double long double
@@ -26,6 +122,7 @@ using namespace std;
 #define vvi vector<vector<int>>
 #define vd vector<double>
 #define vs vector<string>
+#define vll vector<ll>
 template<typename T>
 using min_heap = priority_queue<T, vector<T>, greater<T>>;
 #define fastnuces ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);int t = 1;
@@ -34,7 +131,7 @@ using min_heap = priority_queue<T, vector<T>, greater<T>>;
 void solve(){
 $0
 }
-int main() {
+int32_t main() {
     fastnuces;
     cin >> t;
     while (t--) {
@@ -42,3 +139,9 @@ int main() {
     }
     return 0;
 }
+]]></content>
+  <tabTrigger>ash</tabTrigger>
+  <description></description>
+  <!-- Optional: Set a scope to limit where the snippet will trigger -->
+   <scope >source.c++</scope >
+</snippet>
